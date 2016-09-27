ec1219f5e7d440cae9708c36c9f543ad706bdc49
UT-AliZiyaan <ziyaan@icloud.com>
2016-09-27 14:44:53 -0500

    changes to comments

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -1,14 +1,14 @@
 /* WORD LADDER Main.java
  * EE422C Project 3 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
- * <Student1 5-digit Unique No.>
+ * <Ali Ziyaan Momin>
+ * <AZM259>
+ * <16470>
  * <Student2 Name>
  * <Student2 EID>
- * <Student2 5-digit Unique No.>
+ * <16470>
  * Slip days used: <0>
- * Git URL:
+ * Git URL: https://github.com/AABabber/EE_422C_Project3.git
  * Fall 2016
  */
 
